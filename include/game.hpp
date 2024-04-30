9da3dbffec027132d4f96bfe34d54d28d9d355df
Alex Pacheco <ap112095@gmail.com>
2024-04-29 20:27:16 -0400

    includes and basic hpp outline

---
+++ include/game.hpp
@@ -0,0 +1,12 @@
+#ifdef GAME_HPP
+#define GAME_HPP
+
+#include <vector>
+
+class 2048
+{
+public:
+    vector<vector<int>> board;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "game.cpp"
+
+int main(int argc, char *argv[])
+
+{
+
+    return 0;
+}
